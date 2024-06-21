93c8811a06615d524cf648b72768281379c9e5dc
Josevan Oliveira <josivantarcio@gmail.com>
2024-06-21 04:32:47 -0300

    Update CustomerRepository.java
    remove comment

--- springboot-thymeleaf-web-app/src/main/java/net/springboot/javaguides/repository/CustomerRepository.java
+++ springboot-thymeleaf-web-app/src/main/java/net/springboot/javaguides/repository/CustomerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import net.springboot.javaguides.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
-	 // spring data jpa provides full crud operations for Customer entity
+	 
 }
